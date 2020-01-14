604ea9daa7308784ef3011fa16010f98625f4483
rajsandhu <rajsandhu@gmail.com>
2020-01-14 15:35:22 +0100

    hopefully this commits the staged flaschard.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Flashcard from "./flashcard.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flashcard
+        question="Sample Question Term"
+        answer="Sample Answer"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Clouds_over_the_Atlantic_Ocean.jpg/1024px-Clouds_over_the_Atlantic_Ocean.jpg"
+      />
     </div>
   );
 }
